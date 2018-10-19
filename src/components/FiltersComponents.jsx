70d3109c8b14c3e2028a13537b17c93b63094b13
larbibaraka <laridev@gmail.com>
2018-10-19 23:10:56 +0100

    creating filtersComponents

---
+++ src/components/FiltersComponents.jsx
@@ -0,0 +1,228 @@
+import React, { Component } from 'react'
+import SolidBackground from './SolidBackground';
+import RangeItem  from './RangeItem';
+class FiltersComponents extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        blur : 0,
+        hueRotate : 0,
+        invert : 0,
+        grayScale : 0,
+        sepia : 0,
+        saturation : 0,
+        brightness : 0 ,
+        contrast : 0,
+        none : true,
+        solidBackground : false,
+        linearGradient  : false,
+        radialGradient  : false,
+     }
+  }
+
+  onChange = (e)=>{
+      console.log('--->' , e.target.name) 
+
+      switch (e.target.name) {
+        case 'none':
+          this.setState({none:true, solidBackground : false , linearGradient : false, radialGradient : false});
+          break;
+        case 'solidBackground' :
+          this.setState({none:false, solidBackground : true , linearGradient : false, radialGradient : false});
+          break;
+        case 'linearGradient':
+          this.setState({none:false, solidBackground : false , linearGradient : true, radialGradient : false});
+          break;
+        case 'radialGradient' :
+          this.setState({none:false, solidBackground : false , linearGradient : false, radialGradient : true});
+          break;  
+        default:
+          break;
+      }
+
+      this.setState(
+        {
+          [e.target.name] : e.target.value,
+        }
+      )   
+  }
+
+  onClick = () =>{
+    this.setState({
+        blur : 0,
+        hueRotate : 0,
+        invert : 0,
+        grayScale : 0,
+        sepia : 0,
+        saturation : 0,
+        brightness : 0 ,
+        contrast : 0,
+        none : true,
+        solidBackground : false,
+        linearGradient  : false,
+        radialGradient  : false,
+    })
+  }
+
+ 
+ 
+  render() {
+    return (
+      <nav className="panel">
+          <div  className="panel-heading">
+              <h1 className="title is-4 is-centered">Settings</h1>
+              <p 
+              className="button is-danger is-centered"
+              onClick={this.onClick}
+              >
+              ClearAll</p>
+          </div>
+          <div className="panel-block"> 
+           <RangeItem
+              value={this.state.blur}
+              name = "blur"
+              min  = "0"
+              max  = "10"
+              onChange = {this.onChange}
+              unit = "px"
+              label = "Blur"
+            />
+          </div>
+          <div className="panel-block"> 
+            <RangeItem
+              value={this.state.hueRotate}
+              name = "hueRotate"
+              min  = "0"
+              max  = "360"
+              onChange = {this.onChange}
+              unit = "dg"
+              label = "Hue Rotate"
+            />
+
+
+          </div>
+          <div className="panel-block"> 
+             <RangeItem
+              value={this.state.invert}
+              name = "invert"
+              min  = "0"
+              max  = "100"
+              onChange = {this.onChange}
+              unit = "%"
+              label = "Invert"
+            />
+          </div>
+
+          <div className="panel-block"> 
+            <RangeItem
+              value={this.state.grayScale}
+              name = "grayScale"
+              min  = "0"
+              max  = "100"
+              onChange = {this.onChange}
+              unit = "%"
+              label = "Grayscale"
+            />
+          </div>
+          <div className="panel-block"> 
+            <RangeItem
+              value={this.state.sepia}
+              name = "sepia"
+              min  = "0"
+              max  = "100"
+              onChange = {this.onChange}
+              unit = "%"
+              label = "Sepia"
+            />
+          </div>
+
+          <div className="panel-block"> 
+            <RangeItem
+              value={this.state.saturation}
+              name = "saturation"
+              min  = "0"
+              max  = "200"
+              onChange = {this.onChange}
+              unit = "%"
+              label = "Saturation"
+            />
+
+          </div>
+          <div className="panel-block"> 
+            <RangeItem
+              value={this.state.brightness}
+              name = "brightness"
+              min  = "0"
+              max  = "200"
+              onChange = {this.onChange}
+              unit = "%"
+              label = "Brightness"
+            />
+          </div>
+          <div className="panel-block"> 
+            <RangeItem
+              value={this.state.contrast}
+              name = "contrast"
+              min  = "0"
+              max  = "200"
+              onChange = {this.onChange}
+              unit = "%"
+              label = "Contrast"
+            />
+          </div>
+          <div className="panel-block"> 
+            <div className="div-wrapper">
+              <label className="label">OVERLAY :</label>
+              <div className="field">
+                <input 
+                  className="is-" 
+                  type="radio" 
+                  name="none" 
+                  checked={this.state.none} 
+                  onChange={this.onChange}
+                  />
+                <label >None</label>
+              </div>
+              <div className="field">
+                <input 
+                  className="is-" 
+                  type="radio" 
+                  name="solidBackground" 
+                  checked={this.state.solidBackground}
+                  onChange={this.onChange}
+                 
+                />
+                <label >Solid Background</label>
+              </div>
+              <div className="field">
+                <input 
+                  className="is-" 
+                  type="radio" 
+                  name="linearGradient" 
+                  checked={this.state.linearGradient}
+                  onChange={this.onChange}
+                   
+                />
+                <label >Linear Gradient</label>
+              </div>
+              <div className="field">
+                <input 
+                  className="is-" 
+                  type="radio" 
+                  name="radialGradient" 
+                  checked={this.state.radialGradient}
+                  onChange={this.onChange}
+                />
+                <label>Radial Gradient</label>
+              </div>
+              <div className="div-wrapper">
+                <SolidBackground/>
+              </div>
+            </div>
+          </div>
+       </nav>
+    )
+  }
+}
+
+export default FiltersComponents;
