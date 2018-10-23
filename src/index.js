d589b870e50d688d92d5092712a88ff72b0ea2ae
larbibaraka <laridev@gmail.com>
2018-10-23 22:03:50 +0100

    adding reducer

--- src/index.js
+++ src/index.js
@@ -7,18 +7,22 @@ import * as serviceWorker from './serviceWorker';
 /** redux part */
 import {Provider} from 'react-redux';
 import { createStore , applyMiddleware } from "redux";
-import { CreateLogger } from "redux-logger";
+import { createLogger } from "redux-logger";
 import thunk from 'redux-thunk';
 
 /** reducer*/
 import rootReducer from './reducers';
 
 
-const middleware = [thunk , CreateLogger(rootReducer)];
+const middlaware = [thunk , createLogger(rootReducer)];
 
-const store = createStore(rootReducer, {} , applyMiddleware(...middleware));
+const store = createStore(rootReducer , {} , applyMiddleware(...middlaware) )
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+              <Provider store = {store}>
+                <App />
+              </Provider> , 
+              document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import  FilterReducer from './FiltersReducer';
+export default combineReducers({
+  FilterReducer : FilterReducer
+});
